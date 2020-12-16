cded4e972a7bd4e54f9ff989da6beba0ddeafe01
Kara Caputo <kncaputo@gmail.com>
2020-12-16 12:10:33 -0500

    Memoize endpoint

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,15 @@
+const endpoint = http://localhost:3001/api/v1/reservations;
+
 export const fetchReservations = () => {
-  return fetch('http://localhost:3001/api/v1/reservations')
+  return fetch(endpoint)
   .then((response) => {
     if (!response.ok) {
       throw Error('Failed to fetch reservations')
     }
     return response.json()
   })
+}
+
+export const postReservation = (reservation) => {
+  return fetch(endpoint, )
 }
